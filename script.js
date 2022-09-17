630670fab41e6710a430f86cf882240737df6294
pauljsyi <pauljsyi@gmail.com>
2022-09-16 17:31:28 -0700

    set up increment function to update individual like buttons

--- script.js
+++ script.js
@@ -1,16 +1,29 @@
 console.log("loaded......");
-let currentLikes = 0;
-function incrementLike(e) {
-  // const click = document.querySelector("#like-button");
-  // click.addEventListener("click", function () {
-  //   currentLikes += 1;
-  //   console.log("currentLikes: ", currentLikes);
-  //   let likes = document.querySelector("#likes");
-  //   likes.innerText = currentLikes + "  like(s)";
-  // });
 
-  currentLikes += 1;
-  console.log("currentLikes: ", currentLikes);
-  let likes = document.querySelector("#likes");
-  likes.innerText = currentLikes + "  like(s)";
+let currentLikes = {
+  neilLikes: 0,
+  nicholeLikes: 0,
+  jimLikes: 0,
+};
+
+function incrementLike(e) {
+  if (e.id === document.querySelector("#like-button1").id) {
+    currentLikes.neilLikes++;
+    console.log("neil's likes: ", currentLikes.neilLikes);
+
+    let neil = document.querySelector("#likes1");
+    neil.innerText = currentLikes.neilLikes + " like(s)";
+  } else if (e.id === document.querySelector("#like-button2").id) {
+    currentLikes.nicholeLikes++;
+    console.log("nichole's likes: ", currentLikes.nicholeLikes);
+
+    let nichole = document.querySelector("#likes2");
+    nichole.innerText = currentLikes.nicholeLikes + " like(s)";
+  } else if (e.id === document.querySelector("#like-button3").id) {
+    currentLikes.jimLikes++;
+    console.log("Jim's likes: ", currentLikes.jimLikes);
+
+    let nichole = document.querySelector("#likes3");
+    nichole.innerText = currentLikes.jimLikes + " like(s)";
+  }
 }
